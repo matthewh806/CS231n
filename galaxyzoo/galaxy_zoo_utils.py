02e2dd029117954efca2371ac73d31b0dcfe44eb
Matthew <matthewh806@gmail.com>
2016-07-24 21:16:44 +0200

    Tidied up method for retrieving galaxy zoo data

--- galaxyzoo/galaxy_zoo_utils.py
+++ galaxyzoo/galaxy_zoo_utils.py
@@ -1,34 +1,24 @@
 import os
+import json
 import download_kaggle_data
 
-training_images_url = \
-    'https://www.kaggle.com/c/galaxy-zoo-the-galaxy-challenge/download/images_training_rev1.zip'
-training_solutions_url = \
-    'https://www.kaggle.com/c/galaxy-zoo-the-galaxy-challenge/download/training_solutions_rev1.zip'
-test_images_url = \
-        'https://www.kaggle.com/c/galaxy-zoo-the-galaxy-challenge/download/images_test_rev1.zip'
-
 def load_galaxy_zoo(root_dir):
-    training_images_path = os.path.join(root_dir, 'images_training_rev1')
-    training_solutions_path = os.path.join(root_dir,
-                                           'training_solutions_rev1.csv')
-    test_images_path = os.path.join(root_dir, 'images_test_rev1')
+    with open('config.json') as json_data_file:
+        data = json.load(json_data_file)
+    kaggle_data = data["kaggle"]
+    username = kaggle_data["username"]
+    password = kaggle_data["password"]
+    urls = kaggle_data["data_urls"]
 
-    if not os.path.exists(training_images_path):
-        print "%s path not found, downloading data..." % training_images_path
-        download_kaggle_data.download_data(training_images_url, username,
-                                           'password', root_dir)
-    if not os.path.exists(training_solutions_path):
-        print "%s path not found, downloading data..." % training_solutions_path
-        download_kaggle_data.download_data(training_solutions_url,
-                                           'username',
-                                           'password', root_dir)
-    if not os.path.exists(test_images_path):
-        print "%s path not found, downloading data..." % test_images_path
-        download_kaggle_data.download_data(test_images_url, 'username',
-                                           'password', root_dir)
-
+    for url in urls:
+        # Check if file / dir name exists 
+        name = os.path.splitext(url.split("/")[-1])[0] 
+        path = os.path.join(root_dir, name)
+        if not os.path.exists(path):
+            print "%s path not found, downloading data..." % path
+            download_kaggle_data.download_data(url, username,
+                                           password, root_dir)
     print "All required data downloaded"
 
 if __name__ == "__main__":
-    load_galaxy_zoo('datasets/galaxyzoo')
+    load_galaxy_zoo('datasets')
